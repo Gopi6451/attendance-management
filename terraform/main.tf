b03d379e4a2199080f54026e954f65c3058a43a5
Gopi6451 <gopikanth6451@gmail.com>
2026-09-03 15:30:14 +0530

    Test Kubernetes deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,6 +20,7 @@ resource "aws_vpc" "attendance_vpc" {
   }
 }
 
+
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.attendance_vpc.id
   cidr_block              = "10.0.1.0/24"
